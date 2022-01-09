6ec477750f336d08983ca967f95a230f51b5081a
Shahriarshifat <malam181168@bscse.uiu.ac.bd>
2022-01-09 22:02:15 +0600

    Code editing

--- SoftProject.java
+++ SoftProject.java
@@ -5,11 +5,19 @@ class Main {
     System.out.println("Enter two numbers");
     int first = 10;
     int second = 20;
+    float first = 1.5f;
+    float second = 2.0f;
     
     System.out.println(first + " " + second);
 
     
     int sum = first + second;
     System.out.println("The sum is: " + sum);
+
+
+    float product = first * second;
+
+     System.out.println("The product is: " + product);
+
   }
 }
